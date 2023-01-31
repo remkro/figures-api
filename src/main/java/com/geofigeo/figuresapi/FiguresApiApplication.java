c5cdd2d7676cf5a9b4a1f06a8bc7989e67b6564d
Remigiusz Królak <krolak.remek@gmail.com>
2023-01-31 12:28:59 +0100

    Created project, added dependencies, configured h2 database connection

---
+++ src/main/java/com/geofigeo/figuresapi/FiguresApiApplication.java
@@ -0,0 +1,13 @@
+package com.geofigeo.figuresapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FiguresApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FiguresApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/geofigeo/figuresapi/FiguresApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.geofigeo.figuresapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FiguresApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
