903c3ec5d0d346505a5e221d9491124b66561723
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-13 13:42:06 +0100

    Secured endpoints to be available for ADMIN and resource creators

---
+++ src/main/java/com/geofigeo/figuresapi/security/UserSecurity.java
@@ -0,0 +1,24 @@
+package com.geofigeo.figuresapi.security;
+
+import com.geofigeo.figuresapi.entities.Shape;
+import com.geofigeo.figuresapi.repositories.ShapeRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Component;
+
+@Component("userSecurity")
+@RequiredArgsConstructor
+public class UserSecurity {
+    private final ShapeRepository shapeRepository;
+
+    public boolean isResourceCreator(Long shapeId) {
+        boolean present = shapeRepository.findById(shapeId).isPresent();
+        if(!present) {
+            return false;
+        }
+        Shape shape = shapeRepository.findById(shapeId).get();
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        return authentication.getName().equalsIgnoreCase(shape.getCreatedBy());
+    }
+}
