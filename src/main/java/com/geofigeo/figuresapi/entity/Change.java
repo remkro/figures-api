68a5d0793cf36a6215bbbb06b7b5e7f4723a7174
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-14 20:36:40 +0100

    Replaced with single class imports

--- src/main/java/com/geofigeo/figuresapi/entity/Change.java
+++ src/main/java/com/geofigeo/figuresapi/entity/Change.java
@@ -1,6 +1,15 @@
 package com.geofigeo.figuresapi.entity;
 
-import jakarta.persistence.*;
+import jakarta.persistence.CollectionTable;
+import jakarta.persistence.Column;
+import jakarta.persistence.ElementCollection;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.MapKeyColumn;
+import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
