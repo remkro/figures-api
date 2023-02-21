aeed462ecfcb211d54b3811c3debb84090d35ba9
Remigiusz Królak <krolak.remek@gmail.com>
2023-02-21 11:11:40 +0100

    Used optional chain for isResourceCreator validation

--- src/main/java/com/geofigeo/figuresapi/security/UserSecurity.java
+++ src/main/java/com/geofigeo/figuresapi/security/UserSecurity.java
@@ -2,11 +2,8 @@ package com.geofigeo.figuresapi.security;
 
 import com.geofigeo.figuresapi.repository.ShapeRepository;
 import lombok.RequiredArgsConstructor;
-import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Component;
-
-import java.util.Optional;
 
 @Component("userSecurity")
 @RequiredArgsConstructor
@@ -14,12 +11,7 @@ public class UserSecurity {
     private final ShapeRepository shapeRepository;
 
     public boolean isResourceCreator(Long shapeId) {
-        Optional<String> createdByOptional = shapeRepository.getCreatedByById(shapeId);
-        if (createdByOptional.isEmpty()) {
-            return false;
-        } else {
-            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-            return authentication.getName().equalsIgnoreCase(createdByOptional.get());
-        }
+        return shapeRepository.getCreatedByById(shapeId).map(username -> SecurityContextHolder.getContext()
+                .getAuthentication().getName().equalsIgnoreCase(username)).orElse(false);
     }
 }
