1e1d3f13aee0eb25f9e30cb2ee843d548eeb87a5
Remigiusz Królak <krolak.remek@gmail.com>
2023-03-14 20:29:54 +0100

    Change cascade types for Roles in User entity

--- src/main/java/com/geofigeo/figuresapi/entity/User.java
+++ src/main/java/com/geofigeo/figuresapi/entity/User.java
@@ -31,7 +31,7 @@ public class User {
     private String email;
     private String password;
 
-    @ManyToMany(fetch = FetchType.LAZY,cascade = CascadeType.ALL)
+    @ManyToMany(fetch = FetchType.LAZY,cascade = {CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     @JoinTable(name = "user_roles",
             joinColumns = @JoinColumn(name = "user_id", referencedColumnName = "id"),
             inverseJoinColumns = @JoinColumn(name = "role_id", referencedColumnName = "id"))
